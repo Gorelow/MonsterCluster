d20a62faa34bb92317624c86d4961fbfc3ac45aa
Gorelow <50836223+Gorelow@users.noreply.github.com>
2023-04-11 11:04:03 +0300

    UnitUI refactor

--- Assets/Scripts/View/UnitUI.cs
+++ Assets/Scripts/View/UnitUI.cs
@@ -10,66 +10,62 @@ namespace View
     {
         public static readonly int SHOW_TIME = 5000;
         public static readonly int REAPPEAR_DELAY = 200;
-        public static readonly string APPEAR_TRIGGER = "OnAppear";
-        public static readonly string REAPPEAR_TRIGGER = "OnReappear";
-        public static readonly string DISAPPEAR_TRIGGER = "OnDisappear";
 
         [SerializeField] private Animator _animator;
         [SerializeField] private bool _isAnimating = false;
-        /// <summary>
-        /// Для того чтобы показать инфу о персонаже нужно:
-        /// 1. Изображение юнита
-        /// 2. Здоровье юнита
-        /// 3. Имя
-        /// 4. Скорость
-        /// 5. Энергия 
-        /// </summary>
+        
         [Header("Info to show")]
         [SerializeField] private Image _image;
         [SerializeField] private TextMeshProUGUI _name;
         [SerializeField] private Slider _health;
         [SerializeField] private TextMeshProUGUI _speed;
         [SerializeField] private TextMeshProUGUI _energy;
+        
+        private int _overlapCounter = 0;
 
-        /// <summary>
-        /// Для aнимирования требуется:
-        /// Аниматор,
-        /// Переменная показывающая показывает ли сейчас аниматор что-то или нет
-        /// </summary>
-
-        private int overlapCounter = 0;
+        private static readonly int OnAppear = Animator.StringToHash("OnAppear");
+        private static readonly int OnReappear = Animator.StringToHash("OnReappear");
+        private static readonly int OnDisappear = Animator.StringToHash("OnDisappear");
 
         public async void ShowInfo(IUnitController unit)
         {
-            overlapCounter++;
+            _overlapCounter++;
+            
+            await Task.Run(AnimateAppearance);
+            UpdateUnitBaseInfo(unit);
+            await Task.Delay(SHOW_TIME);
+            
+            _overlapCounter--;
+            if (_overlapCounter > 0) return;
+            RemoveInfo();
+        }
 
+        private async void AnimateAppearance()
+        {
             if (_isAnimating)
             {
-                _animator.SetTrigger(REAPPEAR_TRIGGER);
+                _animator.SetTrigger(OnReappear);
                 await Task.Delay(REAPPEAR_DELAY);
             }
             else
             {
-                _animator.SetTrigger(APPEAR_TRIGGER);
+                _animator.SetTrigger(OnAppear);
                 _isAnimating = true;
             }
+        }
+
+        private void UpdateUnitBaseInfo(IUnitController unit)
+        {
             _image.sprite = unit.Image;
             _name.text = unit.Name;
             _health.value = unit.Health;
             _speed.text = unit.Initiative.ToString();
             _energy.text = unit.Speed.ToString();
-
-            await Task.Delay(SHOW_TIME);
-
-            overlapCounter--;
-            if (overlapCounter > 0) return;
-
-            RemoveInfo();
         }
 
         public void RemoveInfo()
         {
-            _animator.SetTrigger(DISAPPEAR_TRIGGER);
+            _animator.SetTrigger(OnDisappear);
             _isAnimating = false;
         }
     }
